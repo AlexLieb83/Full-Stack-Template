4dd81f465ea910dbd5171647848ba4b6c359aaba
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-06-24 11:39:30 -0500

    added var and Mongodb

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const app = express();
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
+
+let db = db,
+  dbConnectionString = process.env.DB_STRING,
+  dbName = "",
+  collection;
+
+MongoClient.connect(dbConnectionString).then((client) => {
+  console.log("Connected to Database");
+  db = client.db("templateDB");
+  collection = db.collection("test");
+});
