de933abb3b48af932688d9e37694c020297ed3aa
Alex Liebhaber <alexliebhaber1992@gmail.com>
2022-06-24 10:43:39 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+//require Dependencies - so we can access and use them
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
